585626ba272d68ca1b74c4b00ecb489aa85677ce
Dailey Dai <dailey.yet@gmail.com>
2017-02-27 17:31:36 +0800

    update

--- src/main/java/com/openthinks/webscheduler/controller/ProfileController.java
+++ src/main/java/com/openthinks/webscheduler/controller/ProfileController.java
@@ -1,28 +1,24 @@
-/**   
- *  Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
-* @Title: ProfileController.java 
-* @Package com.openthinks.webscheduler.controller 
-* @Description: TODO
-* @author dailey.yet@outlook.com  
-* @date Sep 27, 2016
-* @version V1.0   
-*/
+ * @Title: ProfileController.java
+ * @Package com.openthinks.webscheduler.controller
+ * @Description: TODO
+ * @author dailey.yet@outlook.com
+ * @date Sep 27, 2016
+ * @version V1.0
+ */
 package com.openthinks.webscheduler.controller;
 
 import java.util.Optional;
@@ -49,92 +45,98 @@ import com.openthinks.webscheduler.service.WebSecurityService;
 @Controller("/profile")
 public class ProfileController {
 
-	@AutoComponent
-	WebSecurityService securityService;
+  @AutoComponent
+  WebSecurityService securityService;
 
-	private Optional<User> getCurrentUser(WebAttributers was) {
-		User currentUser = was.getSession(StaticDict.SESSION_ATTR_LOGIN_INFO);
-		if (currentUser != null) {
-			currentUser = securityService.getUsers().findById(currentUser.getId());
-			return Optional.of(currentUser);
-		}
-		return Optional.empty();
-	}
+  private Optional<User> getCurrentUser(WebAttributers was) {
+    User currentUser = was.getSession(StaticDict.SESSION_ATTR_LOGIN_INFO);
+    if (currentUser != null) {
+      currentUser = securityService.getUsers().findById(currentUser.getId());
+      return Optional.of(currentUser);
+    }
+    return Optional.empty();
+  }
 
-	@Mapping("/index")
-	public String index(WebAttributers was) {
+  @Mapping("/index")
+  public String index(WebAttributers was) {
 
-		Optional<User> currentUser = getCurrentUser(was);
-		if (!currentUser.isPresent()) {
-			was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "Session timeout, please login again.", WebScope.REQUEST);
-			return StaticUtils.errorPage(was, PageMap.build());
-		}
-		was.storeRequest(StaticDict.PAGE_ATTRIBUTE_USER, currentUser.get());
-		return "WEB-INF/jsp/profile.jsp";
-	}
+    Optional<User> currentUser = getCurrentUser(was);
+    if (!currentUser.isPresent()) {
+      was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "Session timeout, please login again.",
+          WebScope.REQUEST);
+      return StaticUtils.errorPage(was, PageMap.build());
+    }
+    was.storeRequest(StaticDict.PAGE_ATTRIBUTE_USER, currentUser.get());
+    return "WEB-INF/jsp/profile.jsp";
+  }
 
-	@Mapping("/base/edit")
-	public String editBaseInfo(WebAttributers was) {
-		String email = was.get(StaticDict.PAGE_PARAM_USER_EMAIL);
-		try {
-			Checker.require(email).notEmpty("User email can not be empty.");
-		} catch (Exception e) {
-			was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, e.getMessage(), WebScope.REQUEST);
-			return StaticUtils.errorPage(was, PageMap.build());
-		}
-		Optional<User> currentUser = getCurrentUser(was);
+  @Mapping("/base/edit")
+  public String editBaseInfo(WebAttributers was) {
+    String email = was.get(StaticDict.PAGE_PARAM_USER_EMAIL);
+    try {
+      Checker.require(email).notEmpty("User email can not be empty.");
+    } catch (Exception e) {
+      was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, e.getMessage(), WebScope.REQUEST);
+      return StaticUtils.errorPage(was, PageMap.build());
+    }
+    Optional<User> currentUser = getCurrentUser(was);
 
-		if (!currentUser.isPresent()) {
-			was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "Session timeout, please login again.", WebScope.REQUEST);
-			return StaticUtils.errorPage(was, PageMap.build());
-		}
-		User user = currentUser.get();
-		if (!email.equals(user.getEmail())) {
-			User duplicateUser = securityService.getUsers().findByEmail(email);
-			if (duplicateUser != null) {
-				was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1,
-						"Couldn't update profile, maybe this email has already been used.", WebScope.REQUEST);
-				return StaticUtils.errorPage(was, PageMap.build());
-			}
-		}
-		user.setEmail(email);
-		securityService.saveToDisk();
-		return WebUtils.redirect("/profile/index");
-	}
+    if (!currentUser.isPresent()) {
+      was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "Session timeout, please login again.",
+          WebScope.REQUEST);
+      return StaticUtils.errorPage(was, PageMap.build());
+    }
+    User user = currentUser.get();
+    if (!email.equals(user.getEmail())) {
+      User duplicateUser = securityService.getUsers().findByEmail(email);
+      if (duplicateUser != null) {
+        was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1,
+            "Couldn't update profile, maybe this email has already been used.", WebScope.REQUEST);
+        return StaticUtils.errorPage(was, PageMap.build());
+      }
+    }
+    user.setEmail(email);
+    securityService.saveToDisk();
+    return WebUtils.redirect("/profile/index");
+  }
 
-	@Mapping("/pwd/edit")
-	public String editPwd(WebAttributers was) {
-		String oldPwd = was.get(StaticDict.PAGE_PARAM_USER_OLD_PWD);
-		String newPwd = was.get(StaticDict.PAGE_PARAM_USER_NEW_PWD);
-		String newPwdAgain = was.get(StaticDict.PAGE_PARAM_USER_NEW_PWD_AGAIN);
+  @Mapping("/pwd/edit")
+  public String editPwd(WebAttributers was) {
+    String oldPwd = was.get(StaticDict.PAGE_PARAM_USER_OLD_PWD);
+    String newPwd = was.get(StaticDict.PAGE_PARAM_USER_NEW_PWD);
+    String newPwdAgain = was.get(StaticDict.PAGE_PARAM_USER_NEW_PWD_AGAIN);
 
-		try {
-			Checker.require(oldPwd).notEmpty("Old user password can not be empty.");
-			Checker.require(newPwd).notEmpty("New user password can not be empty.");
-			Checker.require(newPwdAgain).notEmpty("New user password again can not be empty.");
-		} catch (Exception e) {
-			was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, e.getMessage(), WebScope.REQUEST);
-			return StaticUtils.errorPage(was, PageMap.build());
-		}
-		Optional<User> currentUser = getCurrentUser(was);
-		if (!currentUser.isPresent()) {
-			was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "Session timeout, please login again.", WebScope.REQUEST);
-			return StaticUtils.errorPage(was, PageMap.build());
-		}
-		User user = currentUser.get();
-		if (!user.getPass().equals(DigestUtils.md5Hex(oldPwd))) {
-			was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "Invalid old password.", WebScope.REQUEST);
-			return StaticUtils.errorPage(was, PageMap.build());
-		}
+    try {
+      Checker.require(oldPwd).notEmpty("Old user password can not be empty.");
+      Checker.require(newPwd).notEmpty("New user password can not be empty.");
+      Checker.require(newPwdAgain).notEmpty("New user password again can not be empty.");
+    } catch (Exception e) {
+      was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, e.getMessage(), WebScope.REQUEST);
+      return StaticUtils.errorPage(was, PageMap.build());
+    }
+    Optional<User> currentUser = getCurrentUser(was);
+    if (!currentUser.isPresent()) {
+      was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "Session timeout, please login again.",
+          WebScope.REQUEST);
+      return StaticUtils.errorPage(was, PageMap.build());
+    }
+    User user = currentUser.get();
+    if (!user.getPass().equals(DigestUtils.md5Hex(oldPwd))) {
+      was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "Invalid old password.", WebScope.REQUEST);
+      return StaticUtils.errorPage(was, PageMap.build());
+    }
 
-		if (!newPwd.equals(newPwdAgain)) {
-			was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1, "The twice new password are not same, please check.",
-					WebScope.REQUEST);
-			return StaticUtils.errorPage(was, PageMap.build());
-		}
-		user.setPass(DigestUtils.md5Hex(newPwd));
-		securityService.saveToDisk();
-		return WebUtils.redirect("/security/logout");
-	}
+    if (!newPwd.equals(newPwdAgain)) {
+      was.addError(StaticDict.PAGE_ATTRIBUTE_ERROR_1,
+          "The twice new password are not same, please check.", WebScope.REQUEST);
+      return StaticUtils.errorPage(was, PageMap.build());
+    }
+    user.setPass(DigestUtils.md5Hex(newPwd));
+    securityService.saveToDisk();
+    return WebUtils.redirect("/security/logout");
+  }
 
+  public static void main(String[] args) {
+    System.out.println(DigestUtils.md5Hex("123456"));
+  }
 }
--- src/main/java/com/openthinks/webscheduler/task/support/GroupTask.java
+++ src/main/java/com/openthinks/webscheduler/task/support/GroupTask.java
@@ -1,28 +1,24 @@
-/**   
- *  Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
-* @Title: GroupTask.java 
-* @Package com.openthinks.webscheduler.task.support 
-* @Description: TODO
-* @author dailey.yet@outlook.com  
-* @date Aug 29, 2016
-* @version V1.0   
-*/
+ * @Title: GroupTask.java
+ * @Package com.openthinks.webscheduler.task.support
+ * @Description: TODO
+ * @author dailey.yet@outlook.com
+ * @date Aug 29, 2016
+ * @version V1.0
+ */
 package com.openthinks.webscheduler.task.support;
 
 import java.util.Optional;
@@ -48,78 +44,81 @@ import com.openthinks.webscheduler.task.TaskRefDefinitionDescriber;
  *
  */
 public class GroupTask implements SupportTaskDefinition {
-	TaskService taskService = WebContexts.get().lookup(TaskService.class);
+  TaskService taskService = WebContexts.get().lookup(TaskService.class);
 
-	@Override
-	public void execute(TaskContext context) {
-		TaskRunTimeData taskRunTimeData = getTaskRunTimeData(context).get();
-		ITaskRef taskRef = getTaskRefDescriber().createTaskRef();
-		try {
-			taskRunTimeData.preparedTaskRef(taskRef);
-			Optional<String> ops = taskRef.getProp("task-ids");
-			if (ops.isPresent()) {
-				String taskIds = ops.get();
-				String[] ids = taskIds.split(";");
-				for (String id : ids) {//execute each task 
-					TaskContext subtaskCtx = context.clone();
-					String subResult = executeSubTask(subtaskCtx, id);
-					taskRunTimeData.getLastTaskResult().track(subResult);
-					context.syncTaskRuntimeData();
-				}
-			}
-		} catch (Exception e) {
-			ProcessLogger.error(CommonUtilities.getCurrentInvokerMethod(), e);
-			throw new TaskInterruptException(e);
-		}
-	}
+  @Override
+  public void execute(TaskContext context) {
+    TaskRunTimeData taskRunTimeData = getTaskRunTimeData(context).get();
+    ITaskRef taskRef = getTaskRefDescriber().createTaskRef();
+    try {
+      taskRunTimeData.preparedTaskRef(taskRef);
+      Optional<String> ops = taskRef.getProp("task-ids");
+      if (ops.isPresent()) {
+        String taskIds = ops.get();
+        String[] ids = taskIds.split(";");
+        for (String id : ids) {// execute each task
+          ProcessLogger.debug("Processing subtask[" + id + "]");
+          TaskContext subtaskCtx = context.clone();
+          String subResult = executeSubTask(subtaskCtx, id);
+          taskRunTimeData.getLastTaskResult().track(subResult);
+          context.syncTaskRuntimeData();
+        }
+      }
+    } catch (Exception e) {
+      ProcessLogger.error(CommonUtilities.getCurrentInvokerMethod(), e);
+      throw new TaskInterruptException(e);
+    }
+  }
 
-	/**
-	 * execute task by task id
-	 * @param subContext TaskContext
-	 * @param id String
-	 */
-	protected String executeSubTask(TaskContext subTaskCtx, String id) {
-		String _id = id.trim();
-		TaskRunTimeData trtd = null;
-		StringBuffer sb = new StringBuffer();
-		try {
-			trtd = taskService.getTask(_id);
-			ITaskDefinition taskInstance = InstanceUtilities.create(ITaskDefinition.class,
-					InstanceWrapper.build(trtd.getTaskClass()));
-			subTaskCtx.setTaskRuntimeData(trtd);
-			taskInstance.execute(subTaskCtx);
-			sb.append(StaticUtils.formatNow() + ": Success to execute task[id='" + _id + "',type='" + trtd.getTaskType()
-					+ "'].");
-		} catch (Exception e) {
-			ProcessLogger.warn(CommonUtilities.getCurrentInvokerMethod(), e);
-			if (trtd != null) {
-				sb.append(StaticUtils.formatNow() + ": Failed to execute task[id='" + _id + "',type='"
-						+ trtd.getTaskType() + "']; ");
-			} else {
-				sb.append(StaticUtils.formatNow() + ": Failed to execute task[id='" + _id + "']; ");
-			}
-			sb.append("error message: " + e.getMessage());
-		}
-		return sb.toString();
-	}
+  /**
+   * execute task by task id
+   * 
+   * @param subContext TaskContext
+   * @param id String
+   */
+  protected String executeSubTask(TaskContext subTaskCtx, String id) {
+    String _id = id.trim();
+    TaskRunTimeData trtd = null;
+    StringBuffer sb = new StringBuffer();
+    try {
+      trtd = taskService.getTask(_id);
+      ITaskDefinition taskInstance = InstanceUtilities.create(ITaskDefinition.class,
+          InstanceWrapper.build(trtd.getTaskClass()));
+      subTaskCtx.setTaskRuntimeData(trtd);
+      taskInstance.execute(subTaskCtx);
+      sb.append(StaticUtils.formatNow() + ": Success to execute task[id='" + _id + "',type='"
+          + trtd.getTaskType() + "'].");
+    } catch (Exception e) {
+      ProcessLogger.warn(CommonUtilities.getCurrentInvokerMethod(), e);
+      if (trtd != null) {
+        sb.append(StaticUtils.formatNow() + ": Failed to execute task[id='" + _id + "',type='"
+            + trtd.getTaskType() + "']; ");
+      } else {
+        sb.append(StaticUtils.formatNow() + ": Failed to execute task[id='" + _id + "']; ");
+      }
+      sb.append("error message: " + e.getMessage());
+    }
+    return sb.toString();
+  }
 
-	@Override
-	public TaskDefinitionDescriber getTaskDescriber() {
-		return TaskDefinitionDescriber.build(GroupTask.class)
-				.push("Group other tasks as one task, execute them one by one.");
-	}
+  @Override
+  public TaskDefinitionDescriber getTaskDescriber() {
+    return TaskDefinitionDescriber.build(GroupTask.class)
+        .push("Group other tasks as one task, execute them one by one.");
+  }
 
-	@Override
-	public TaskRefDefinitionDescriber getTaskRefDescriber() {
-		TaskRefDefinitionDescriber describer = DefaultTaskRef.getTaskRefDescriber();
-		describer.setRequired(true);
-		preparedRefDescription(describer);
-		return describer;
-	}
+  @Override
+  public TaskRefDefinitionDescriber getTaskRefDescriber() {
+    TaskRefDefinitionDescriber describer = DefaultTaskRef.getTaskRefDescriber();
+    describer.setRequired(true);
+    preparedRefDescription(describer);
+    return describer;
+  }
 
-	private void preparedRefDescription(TaskRefDefinitionDescriber describer) {
-		describer.push("#[required]task ids, split by semicolon(;)");
-		describer.push("task-ids=0762a2b4-7ae7-4a90-9428-83076bfaa417;03d2a2b4-7ae7-4a90-9428-83076bfaaer58");
-	}
+  private void preparedRefDescription(TaskRefDefinitionDescriber describer) {
+    describer.push("#[required]task ids, split by semicolon(;)");
+    describer.push(
+        "task-ids=0762a2b4-7ae7-4a90-9428-83076bfaa417;03d2a2b4-7ae7-4a90-9428-83076bfaaer58");
+  }
 
 }
